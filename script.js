11555ca7e97fa80fdd6bdc80fb2ebc54fbeb0518
Lewicki <ebvkx@emea.local>
2020-05-23 13:17:35 +0200

    js functionality

---
+++ script.js
@@ -0,0 +1,7 @@
+const rightArrow = document.getElementById('arrow-right')
+const leftArrow = document.getElementById('arrow-left')
+const carusel = document.getElementsByClassName('testimonials')[0]
+
+function rightClick = {
+    
+}
