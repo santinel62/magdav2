9903f6204dcf9639f5023ed75e9d5aa559f06155
marcinburzynski <marcinburzynski@stormymood.me>
2020-05-23 13:37:21 +0200

    added event listener for the right arrow

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const rightArrow = document.getElementById('arrow-right')
 const leftArrow = document.getElementById('arrow-left')
-const carusel = document.getElementsByClassName('testimonials')[0]
+const carousel = document.getElementsByClassName('testimonials')[0]
 
-function rightClick = {
+const rightClick = () => {
+    const maxScrollMove = carousel.scrollWidth - carousel.clientWidth
     
+    if (carousel.scrollLeft + 50 < maxScrollMove) {
+        carousel.scrollLeft = carousel.scrollLeft + 50
+    }
 }
+
+rightArrow.addEventListener('click', rightClick)
+
+
